40c7ff47b582f30b66191d07d2de1dafea75ca2f
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2017-11-26 18:00:11 +0000

    Drone Load function

--- Drone.cs
+++ Drone.cs
@@ -10,12 +10,32 @@ namespace hashCode
     class Drone
     {
         Pos Location { get; set; }
-        Drone(){
+        int MaxWeight { get; }
+        int HeldItems { get; set; }
+        int CurrentWeight { get; set; }
+
+        Drone(int maxWeight){
             Location = new Pos(0, 0);
+            MaxWeight = maxWeight;
         }
         public int TimeToPosition(Pos target)
         {
             return (int) Math.Ceiling(Math.Sqrt(Math.Pow(Location.X - target.X, 2) + Math.Pow(Location.Y - target.Y, 2)));
+        }
+        public bool Load(Pos warehouseLocation, int[] itemsToLoad) {
+            int newWeight = CurrentWeight;
+            for (int i = 0; i < itemsToLoad.Length; i++) {
+                newWeight += ItemWeights[i] * itemsToLoad[i];
+            }
+            if (newWeight > MaxWeight) {
+                return false;
+            }
+            else {
+                for (int i = 0; i < itemsToLoad.Length; i++) {
+                    HeldItems += itemsToLoad[i];
+                }
+                return true;
+            }
         }
     }
 }
