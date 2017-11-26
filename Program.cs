165a686fcd408f58b69cfbaa5d5928a37c1f856f
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2017-11-26 17:14:59 +0000

    Adding position struct and warehouse class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,14 @@
 
 namespace hashCode
 {
+    struct Pos {
+        int X { get; set; }
+        int Y { get; set; }
+        Pos(int x, int y) {
+            X = x;
+            Y = y;
+        }
+    }
 	class MainClass
 	{
 		public static void Main (string[] args)
